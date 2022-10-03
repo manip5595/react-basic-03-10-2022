c5adf3b2fba9f0c274b14acb05091ab98140dc58
Manikandan <manip5595@gmail.com>
2022-10-03 16:33:21 +0530

    new testing changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       
       
       {/* testing */}
+      {/* testing to git */}
        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
